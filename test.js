075ca1dc2bf9f051133834c4d92fa976bac15799
David Mallon ⚙ <mallond@gmail.com>
2022-01-19 19:29:00 +0000

    save me

--- test.js
+++ test.js
@@ -15,7 +15,7 @@ const point = new Point('automated-test-runs')
    .tag('product-family','casino-solutions')
    .tag('product-line', 'aml-compliance')
    .tag('product_module', 'module-1')
-   .stringField('value','passed')
+   .field('result','passed')
 
 console.log(`${point}`)
 writeApi.writePoint(point)
