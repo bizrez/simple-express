24edfcd6847f1e9e1e92e743317fee11ebbb895d
David Mallon ⚙ <mallond@gmail.com>
2022-01-19 20:04:22 +0000

    save me

--- test.js
+++ test.js
@@ -15,7 +15,7 @@ const point = new Point('automated-test-runs')
    .tag('product-family','casino-solutions')
    .tag('product-line', 'aml-compliance')
    .tag('product_module', 'module-1')
-   .stringField('result', 'passed')
+   .stringField('result', 'failed')
    
 
 console.log(`${point}`)
