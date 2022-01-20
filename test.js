97d997fd89833d21ff2f55fde11fcecaf4926bf3
David Mallon ⚙ <mallond@gmail.com>
2022-01-20 21:48:08 +0000

    save

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 const {InfluxDB} = require('@influxdata/influxdb-client')
 
 // You can generate an API token from the "API Tokens Tab" in the UI
-const token = 'IenHfnUY6oHIRn0e7GeVAB29GDBKKFTW'
+const token = 'vo4fTmF_ZIidhT5fsvrX2m39wvd2ZBD5hgiKg8PrecaubfYQsrBpO7Y9cELLM5TLS0rt-QrRVU90ezDgFX6xEQ=='
 const org = 'everi'
 const bucket = 'mallon'
 
-const client = new InfluxDB({url: 'http://20.189.163.219', token: token})
+const client = new InfluxDB({url: 'http://168.61.149.254', token: token})
 const {Point} = require('@influxdata/influxdb-client')
 const writeApi = client.getWriteApi(org, bucket)
 writeApi.useDefaultTags({host: 'everi'})
